ba247b3b2784539608a6a18431ac9541b9456899
”jivan” <q982151756@gmail.com>
2022-04-19 21:34:17 +0800

    接着学习Linux系统编程

--- linux_c/io/stdio/fopen.c
+++ linux_c/io/stdio/fopen.c
@@ -1,12 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<errno.h>
+#include<string.h>
 int main() {
     FILE *fp;
-    fp = fopen();
+    fp = fopen("tmp", "r");
     if(fp == NULL) {
-        fprintf(stderr, "fopen() failed! errno = %d\n", errno);
+        // fprintf(stderr, "fopen() failed! errno = %d\n", errno);
+        
+        //Print a message describing the meaning of the value of errno.
+        // perror("fopen()");
+
+        fprintf(stderr, "fopen():%s\n", strerror(errno));
         exit(1);
     }
     puts("OK!");
+
+    fclose(fp);
     exit(0);
 }
