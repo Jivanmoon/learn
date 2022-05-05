6cf83e4e3fb4475d52855b131925bab00b726abc
”jivan” <q982151756@gmail.com>
2022-05-05 21:08:50 +0800

    哎，最近好不想学习

---
+++ linux_c/file_system/big.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+int main(int argc, char **argv) {
+
+    if(argc < 2) {
+        fprintf(stderr, "...");
+        exit(1);
+    }
+
+    int fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+    if(fd < 0) {
+        perror("open()");
+        exit(1);
+    }
+
+    lseek(fd, 5LL * 1024LL * 1024LL * 1024LL, SEEK_SET);
+    //if error
+    //write(fd, "", 1);
+
+    close(fd);
+    exit(0);
+}
