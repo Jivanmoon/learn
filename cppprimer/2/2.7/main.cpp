c16aa8a9a3443fdfa68c429a80e86f1ff06f4d46
”jivan” <q982151756@gmail.com>
2022-05-29 19:08:37 +0800

    继续完成第二章的课后题

---
+++ cppprimer/2/2.7/main.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout << 3.14e1L << endl;
+    return 0;
+}
---
+++ cppprimer/2/2.8/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    // cout << "\62\115";
+    // cout << "\12";
+
+    cout << '\62';
+    cout << '\t';
+    cout << '\115';
+    cout << '\n';
+    return 0;
+}
