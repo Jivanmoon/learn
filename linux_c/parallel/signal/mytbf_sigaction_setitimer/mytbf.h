60368d5be9bad3f23aeb4522181b3b75ad0992ab
jivanmoon <q982151756@gmail.com>
2022-06-25 19:32:17 +0800

    结束信号部分

---
+++ linux_c/parallel/signal/mytbf_sigaction_setitimer/mytbf.h
@@ -0,0 +1,17 @@
+//实现一个令牌桶，令牌桶用于流量控制
+#ifndef MYTBF_H
+#define MYTBF_H
+
+#define MYTBF_MAX 1024
+
+typedef void mytbf_t;
+
+mytbf_t *mytbf_init(int cps, int burst);
+
+int mytbf_fetchtoken(mytbf_t *, int);
+
+int mytbf_returntoken(mytbf_t *, int);
+
+int mytbf_destory(mytbf_t *);
+
+#endif
