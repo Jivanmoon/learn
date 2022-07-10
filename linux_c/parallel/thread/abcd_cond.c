de535a9ea357a638a184ffd7078ab7854f4d84c4
jivanmoon <q982151756@gmail.com>
2022-07-10 20:14:58 +0800

    多线程部分还挺有意思

--- linux_c/parallel/thread/abcd_cond.c
+++ linux_c/parallel/thread/abcd_cond.c
@@ -3,9 +3,13 @@
 #include<pthread.h>
 #include<string.h>
 #include<unistd.h>
+#include<signal.h>
 
 #define THRNUM 4
-static pthread_mutex_t mut[THRNUM];
+static pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+static pthread_t tid[THRNUM];
+static int num = 0;
 
 static int next(int n) {
     if(n + 1 == THRNUM) return 0;
@@ -16,34 +20,34 @@ static void *thr_func(void *p) {
     int n = (int)p;
     int c = 'a' + n;
     while(1) {
-        pthread_mutex_lock(mut + n);
+        pthread_mutex_lock(&mut);
+        while(num != n)
+            pthread_cond_wait(&cond, &mut);
         write(1, &c, 1);
-        pthread_mutex_unlock(mut + next(n));
+        num = next(num);
+        pthread_cond_broadcast(&cond);
+        pthread_mutex_unlock(&mut);
     }
     pthread_exit(NULL);
 }
 
+
 int main() {
 
-    pthread_t tid[THRNUM];
-
     for(int i = 0; i < THRNUM; ++i) {
-        pthread_mutex_init(mut + i, NULL);
-        pthread_mutex_lock(mut + i); 
         int err = pthread_create(tid + i, NULL, thr_func, (void *)i);
         if(err) {
             fprintf(stderr, "pthread_create():%s\n", strerror(err));
             exit(1);
         }
     }
-
-    pthread_mutex_unlock(mut + 0);
-
-    alarm(5);
+    alarm(2);
 
     for(int i = 0; i < THRNUM; ++i) {
         pthread_join(tid[i], NULL);
     }
 
+    pthread_mutex_destroy(&mut);
+    pthread_cond_destroy(&cond);
     exit(0);
 }
